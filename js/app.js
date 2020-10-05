6fa78b51de9149dcf1cc85d79175b35fc707ada2
adyhariri <mahmoudalhariri1994@gmail.com>
2020-10-05 16:48:44 +0300

    lab201 done , we create guessing game about my self in javascript

---
+++ js/app.js
@@ -0,0 +1,46 @@
+'use strict' ;
+alert('hello and welcome to my website ') ;
+var name = prompt('what is your name ? ') ;
+alert('hello'+name+'let us play guessing game ') ;
+console.log(name);
+alert('your answer must be y/n or yes/no') ;
+var ans =prompt('is my age more than 20 ?').toLowerCase();
+if(ans=='y'||ans=='yes')
+{
+    alert('you are correct');
+}
+else
+alert('your answer wrong');
+console.log(ans);
+ans =prompt('am i from syria ?').toLowerCase();
+if(ans=='y'||ans=='yes')
+{
+    alert('you are correct');
+}
+else
+alert('your answer wrong')
+console.log(ans);
+ans =prompt('is my favorite color nevy ?').toLowerCase();
+if(ans=='y'||ans=='yes')
+{
+    alert('you are correct');
+}
+else
+alert('your answer wrong')
+console.log(ans);
+ans =prompt('do i like cooking ?').toLowerCase();
+if(ans=='n'||ans=='no')
+{
+    alert('you are correct');
+}
+else
+alert('your answer wrong')
+console.log(ans);
+ans =prompt('can i drive a car ?').toLowerCase();
+if(ans=='n'||ans=='no')
+{
+    alert('you are correct');
+}
+else
+alert('your answer wrong')
+console.log(ans);
